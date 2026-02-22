1d65d2bc3ff9774129d63153ac70bca38d5c4207
Rei0133 <reychee05@gmail.com>
2026-02-22 14:00:28 +0800

    Header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header>
+            <h1>Triple R and A</h1>
+            <nav>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Page2</a></li>
+                    <li><a href='#'>Page3</a></li>
+                    <li><a href='#'>Page4</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+
+export default Header;
